aed94e8c9187312ca975dd26c13e97e590798da0
nicklasbns <nicklasbns@gmail.com>
2024-11-05 11:25:11 +0100

    Create listeners

--- client.go
+++ client.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	pb "Consensus/consensus"
+	"fmt"
 	"log"
 	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
+type server struct {
+	pb.UnimplementedConsensusServer
+}
+
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	conn, err := grpc.NewClient("nicklas-laptop.taild2c7d.ts.net:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewConsensusClient(conn)
+
+	lis, err := net.Listen("tcp", "127.0.0.2:50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	s := &server{
-		participants: make(map[string]int64),
-		messages:     []*pb.BroadcastMessage{},
-		clients:      make(map[string]chan *pb.BroadcastMessage), // Initialize the map for client channels
+	s := &server{}
+	pb.RegisterConsensusServer(grpcServer, s)
+
+	fmt.Println("Server is running on port 50051...")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
 	}
-	pb.RegisterChittyChatServer(grpcServer, s)
 }
