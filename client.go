db1a1e4591f4f2d608afbed9e482d9c5a81fcd86
nicklasbns <nicklasbns@gmail.com>
2024-11-05 13:02:59 +0100

    add a Token

--- client.go
+++ client.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"strconv"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -14,8 +16,10 @@ import (
 
 type server struct {
 	pb.UnimplementedConsensusServer
-	address int
-	client  pb.ConsensusClient
+	address       int
+	targetAddress int
+	started       bool
+	client        pb.ConsensusClient
 }
 
 func main() {
@@ -48,20 +52,60 @@ func main() {
 
 }
 
-func (s *server) StartFunction(ctx context.Context, empty *pb.Empty) (*pb.Empty, error) {
+func (s *server) PassToken(ctx context.Context, empty *pb.Token) (*pb.Empty, error) {
+	log.Println("Token recieved")
+	writeToFile()
+
+	s.client.PassToken(context.Background(), &pb.Token{})
+
+	return &pb.Empty{}, nil
+}
+
+func (s *server) StartFunction(ctx context.Context, empty *pb.Empty) (*pb.SuccessStart, error) {
+	if s.started {
+		log.Println("Server already started")
+		return &pb.SuccessStart{
+			Message: "Server " + strconv.Itoa(s.address) + " started",
+		}, nil
+	}
+	s.started = true
 	log.Println("Server " + strconv.Itoa(s.address) + " started")
 
-	connectAddress := "127.0.0." + strconv.Itoa(s.address+1) + ":50051"
+	s.targetAddress = s.address + 1
+	message, client := connectToServer(s.targetAddress)
+	if message == nil {
+		s.targetAddress = 2
+		message, client = connectToServer(s.targetAddress)
+
+	}
+	s.client = client
+
+	log.Println("Connect Success to address" + strconv.Itoa(s.targetAddress))
+	return &pb.SuccessStart{
+		Message: "Server " + strconv.Itoa(s.address) + " started",
+	}, nil
+}
+
+func connectToServer(address int) (*pb.SuccessStart, pb.ConsensusClient) {
+	connectAddress := "127.0.0." + strconv.Itoa(address) + ":50051"
 	conn, err := grpc.NewClient(connectAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
 	defer conn.Close()
 
-	s.client = pb.NewConsensusClient(conn)
-	s.client.StartFunction(context.Background(), &pb.Empty{})
-	log.Println("Connect Success to address" + strconv.Itoa(s.address+1))
-	return &pb.Empty{
-		message: "Server " + strconv.Itoa(s.address) + " started",
-	}, nil
+	client := pb.NewConsensusClient(conn)
+	message, err := client.StartFunction(context.Background(), &pb.Empty{})
+	return message, client
+
+}
+
+func writeToFile() {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	data := []byte(currentTime + " HAHA")
+	err := os.WriteFile("CitricalSection.txt", data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(1 * time.Second)
 }
